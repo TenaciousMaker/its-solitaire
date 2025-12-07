da766a9c40f591ffa9c90d41e4f53199c8895189
TenaciousMaker <TenaciousMaker@users.noreply.github.com>
2025-12-07 12:12:00 -0500

    Add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/AppComponent';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
